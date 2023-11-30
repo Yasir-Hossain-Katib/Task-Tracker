3bdc638a3431354bf4cd4a7315520e0aba1d4842
Yasir-Hossain-Katib <yasirkatib12@gmail.com>
2023-11-30 13:20:08 +0600

    jdhfhd

--- Frontend/src/components/TaskList.jsx
+++ Frontend/src/components/TaskList.jsx
@@ -1,27 +1,63 @@
 // frontend/src/components/TaskList.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import TaskForm from './TaskForm';
 
 const TaskList = () => {
   // State to store the tasks retrieved from the backend
   const [tasks, setTasks] = useState([]);
 
-  // useEffect hook to fetch tasks from the backend when the component mounts
+  
   useEffect(() => {
     // Fetch tasks from the backend using Axios
     axios.get('http://localhost:5000/api/tasks')
       .then(response => setTasks(response.data)) // Update the state with the fetched tasks
       .catch(error => console.error('Error fetching tasks:', error)); // Log an error if the fetch fails
-  }, []); // The empty dependency array ensures that this effect runs only once when the component mounts
+  }, []); 
+
+  const handleUpdate = async(taskId,updatedTaskData) => {
+
+    try{
+
+        console.log('Before Update:', tasks);
+
+        await axios.put(`http://localhost:5000/api/tasks/${taskId}`,updatedTaskData);
+
+        const updatedTasks = await axios.get('http://localhost:5000/api/tasks');
+        setTasks(updatedTasks.data);
+
+
+
+    }catch(error){
+        console.error("error updating task",error);
+    }
+
+    
+
+    
+
+  };
+    
+  
 
   // Render the component
   return (
     <div>
       <h1>Task List</h1>
+      <TaskForm
+        onSubmit={(updatedTaskName) => {
+          // Assuming taskId is obtained from the selected task in the UI
+          const taskId = task._id; // Replace with the actual taskId logic
+          handleUpdate(taskId, { name: updatedTaskName });
+        }}
+      />
       {/* Display the list of tasks */}
       <ul>
         {tasks.map(task => (
-          <li key={task._id}>{task.name}</li>
+          <li key={task._id}>
+            {task.name}
+            <button onClick={() => handleUpdate(task._id)}>Update</button>
+          </li>
         ))}
       </ul>
     </div>
